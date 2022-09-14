8214de9d77f527475c470159197f65c198d3dc53
paullaster <paullasterokoth98@gmail.com>
2022-09-14 09:03:43 +0300

    iterating over map obj

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ let newMap = new Map();
 for (let key in tempObj) {
     newMap.set([key],tempObj[key]);
 }
+for ([key, value] of newMap.entries()){
+    console.log(key, value);
+}
 newMap.set("Name", "Cynthia Ahedo")
 console.log(newMap.delete('Name'));
 console.log(newMap);
