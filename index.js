69bca4d1a249bdc0e7c71e54839b1ba674cfa66e
paullaster <paullasterokoth98@gmail.com>
2022-09-14 13:33:37 +0300

    set & map playground

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ for (let key in tempObj) {
     newMap.set([key],tempObj[key]);
 }
 for ([key, value] of newMap.entries()){
-    //console.log(key, value);
+    //console.table(key, value);
 }
 newMap.set("Name", "Cynthia Ahedo")
 //console.log(newMap.delete('Name'));
@@ -22,6 +22,74 @@ newMap.set("Name", "Cynthia Ahedo")
 /**
  * SET
  */
+let empNo = 0;
+let empStr = "EMPID";
+let objSet = {
+    [empStr + ++empNo]:{
+        empName: 'Peter Smith',
+        position: "Team Lead",
+        project : "glassdoor moderation",
+        dept: "Global Service Delivery",
+        company: "Sama Kenya"
+    },
+    [empStr + ++empNo]:{
+        empName: 'Basil Otieno',
+        position: "Senior Team Lead",
+        project : "glassdoor moderation",
+        dept: "Global Service Delivery",
+        company: "Sama Kenya"
+    },
+    [empStr + ++empNo]:{
+        empName: 'Eunice Wangui',
+        position: "Senior Associate",
+        project : "glassdoor moderation",
+        dept: "Global Service Delivery",
+        company: "Sama Kenya"
+    },
+    [empStr + ++empNo]:{
+        empName: 'Kennedy Njagi',
+        position: "Junior Reviewer Associate",
+        project : "glassdoor moderation",
+        dept: "Global Service Delivery",
+        company: "Sama Kenya"
+    },
+    [empStr + ++empNo]:{
+        empName: 'Isiah Mundia',
+        position: "Senior Associate",
+        project : "glassdoor moderation",
+        dept: "Global Service Delivery",
+        company: "Sama Kenya"
+    },
+    [empStr + ++empNo]:{
+        empName: 'Wamuyu Margaret',
+        position: "Associate",
+        project : "glassdoor moderation",
+        dept: "Global Service Delivery",
+        company: "Sama Kenya"
+    },
+    [empStr + ++empNo]:{
+        empName: 'Paullaster Okoth',
+        position: "Developer",
+        project : "glassdoor moderation",
+        dept: "Global Service Delivery",
+        company: "Sama Kenya"
+    }
+}
+console.table(objSet);
+for (let key in objSet) {
+    newMap.set([key],objSet[key]);
+}
+console.log(newMap);
+for([key, value] of newMap.entries()){
+   // console.table(key, value);
+}
 let newSet = new Set();
-newSet.add("Name", "Cynthia Ahedo");
-console.log(newSet.values("Name"));
+for(let prop in objSet) {
+    newSet.add(prop,objSet[prop]);
+}
+
+for([key,value] of newSet.entries()){
+   // console.table(key,value)
+}
+//newSet.add("Name", "Cynthia Ahedo");
+
